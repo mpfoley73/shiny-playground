11b94991bda423029dd6348834e629c5e82e099c
Michael Foley <mpfoley73@hotmail.com>
2024-12-19 06:09:53 -0500

    Completed lesson 1.

--- App-1/README.md
+++ App-1/README.md
@@ -1,4 +1,22 @@
-# Lesson 1
+# App-1
 
-This app is from [Lesson 1](https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/) of Shiny's Get Started web site.
+[Lesson 1](https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/) of Shiny's *Get Started* web site uses App-1 to introduce Shiny.
 
+Shiny apps are defined in a single script named `app.R` with three components: 
+
+``` r
+ui <- 
+
+server <- function()
+
+shinyApp(ui = ui, server = server)
+```
+
+Run the app from your console with a call to `runApp()`.
+
+``` r
+library(shiny)
+runApp("App-1")
+```
+
+Even better, show the associated code and this README by running in "showcase mode", `runApp("App-1", display.mode = "showcase")`. I modified the example to use ggplot.
--- App-1/app.R
+++ App-1/app.R
@@ -3,4 +3,55 @@
 # 
 #   https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/
 #   
-#  
+
+library(shiny)
+library(bslib)
+library(tidyverse)
+
+# Define UI for app that draws a histogram ----
+ui <- page_sidebar(
+  # App title ----
+  title = "My First Shiny App",
+  # Sidebar panel for inputs ----
+  sidebar = sidebar(
+    # Input: Slider for the number of bins ----
+    sliderInput(
+      inputId = "bins",
+      label = "Number of bins:",
+      min = 1,
+      max = 50,
+      value = 30
+    )
+  ),
+  # Output: Histogram ----
+  plotOutput(outputId = "distPlot")
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    
+    faithful |>
+      ggplot(aes(x = waiting)) +
+      geom_histogram(bins = input$bins, fill = "#007bc2", color = "white") +
+      labs(
+        x = "Waiting time to next eruption (in mins)",
+        title = "My Histogram of waiting times"
+      ) +
+      theme_minimal() +
+      theme(plot.title = element_text(size = 18, color = "dodgerblue4"))
+
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
